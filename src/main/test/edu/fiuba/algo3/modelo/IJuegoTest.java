c3d141ed0807e73fb04303ef707076e9ad857f92
Mafer <mf.pont.tv@gmail.com>
2021-07-01 22:22:58 -0300

    test colocr ejercitos

--- src/main/test/edu/fiuba/algo3/modelo/IJuegoTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/IJuegoTest.java
@@ -4,5 +4,12 @@ public class IJuegoTest {
     test01AgregarJugadores() {
         Juego juego = new Juego();
         juego.agregarJugadores("Pablo", "Avneet");
+        assertEquals(juego.cantidadDeJugadores(), 2);
+    }
+
+    test02ColocarEjercitos() {
+        Juego juego = new Juego();
+        juego.colocarEjercitos("Pablo", 3, "Francia");
+        assertEquals(juego.ejercitosDe("Pablo").size(), 3);
     }
 }
