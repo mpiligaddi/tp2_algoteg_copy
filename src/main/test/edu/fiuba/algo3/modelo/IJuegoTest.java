5d4161abbcdc5d9e190bdec9ad8aa620760c10b3
Mafer <mf.pont.tv@gmail.com>
2021-07-01 22:11:47 -0300

    test fallido de agregar jugadores

---
+++ src/main/test/edu/fiuba/algo3/modelo/IJuegoTest.java
@@ -0,0 +1,8 @@
+package edu.fiuba.algo3.modelo;
+
+public class IJuegoTest {
+    test01AgregarJugadores() {
+        Juego juego = new Juego();
+        juego.agregarJugadores("Pablo", "Avneet");
+    }
+}
