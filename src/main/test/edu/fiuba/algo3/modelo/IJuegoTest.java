b719da6883e3bc9827600471a1912c78d751ecb5
Mafer <mf.pont.tv@gmail.com>
2021-07-03 13:30:03 -0300

    corregir accesibilidad de tests

--- src/main/test/edu/fiuba/algo3/modelo/IJuegoTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/IJuegoTest.java
@@ -5,21 +5,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class IJuegoTest {
     @Test
-    void test01AgregarJugadores() {
+    public void test01AgregarJugadores() {
         Juego juego = new Juego();
         juego.agregarJugadores("Pablo", "Avneet");
         assertEquals(juego.cantidadDeJugadores(), 2);
     }
 
     @Test
-    void test01Agregar4Jugadores() {
+    public void test01Agregar4Jugadores() {
         Juego juego = new Juego();
         juego.agregarJugadores("Pablo", "Avneet", "Sasha", "Sam");
         assertEquals(juego.cantidadDeJugadores(), 4);
     }
 
     @Test
-    void test02ColocarEjercitos() {
+    public void test02ColocarEjercitos() {
         Juego juego = new Juego();
         juego.agregarJugadores("Pablo", "Avneet", "Sasha", "Sam");
         juego.agregarEjercitosAlJugador("Pablo", 3);
