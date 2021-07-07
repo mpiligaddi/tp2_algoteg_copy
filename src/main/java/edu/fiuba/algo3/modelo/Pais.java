db0206587b2fb274bc1a855630d98c0261cd0736
Mafer <mf.pont.tv@gmail.com>
2021-07-07 10:28:59 -0300

    generar dados y calcular ganador sin implementar

--- src/main/java/edu/fiuba/algo3/modelo/Pais.java
+++ src/main/java/edu/fiuba/algo3/modelo/Pais.java
@@ -1,5 +1,11 @@
 package edu.fiuba.algo3.modelo;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+
+import java.util.stream.Collectors;
+
 public class Pais {
     String nombre;
     int ejercitos;
@@ -20,8 +26,21 @@ public class Pais {
         ejercitos += cantidadEjercitos;
 	}
 
-	public void atacar(Pais defensor, int cantSoldados) {
-		//TODO implementar ataque
+	public void atacar(Pais defensor, int cantEjercitos) {
+		List<Integer> dadosAtacante = generarDados(Math.min(cantEjercitos, 3));
+		List<Integer> dadosDefensor = generarDados(Math.min(defensor.ejercitos, 3));
+		calcularGanador(dadosAtacante, dadosDefensor);
 	}
-    
+
+	private int calcularGanador(List<Integer> dadosAtacante, List<Integer> dadosDefensor){
+		return 0;
+	}
+		
+	public List<Integer> generarDados(int soldados){
+		List<Integer> dados = new ArrayList<Integer>();
+		for(int i = 0; i < soldados; i++){
+			dados.add ( (int) Math.random() % 6);
+		}
+		return dados;
+	}
 }
