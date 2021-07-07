f65717c8e050753a5b2afb49a3d121a389358dc1
Mafer <mf.pont.tv@gmail.com>
2021-07-07 12:09:12 -0300

    test fallido calcular ganador

--- src/main/java/edu/fiuba/algo3/modelo/Pais.java
+++ src/main/java/edu/fiuba/algo3/modelo/Pais.java
@@ -1,17 +1,17 @@
 package edu.fiuba.algo3.modelo;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
 
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Pais {
     String nombre;
     int ejercitos;
 
 	public Pais(String n) {
-        this.nombre = n;
+		this.nombre = n;
+		this.ejercitos = 0;
 	}
 
 	public String obtenerNombre() {
@@ -26,21 +26,38 @@ public class Pais {
         ejercitos += cantidadEjercitos;
 	}
 
-	public void atacar(Pais defensor, int cantEjercitos) {
+	public Boolean atacar(Pais defensor, int cantEjercitos) {
 		List<Integer> dadosAtacante = generarDados(Math.min(cantEjercitos, 3));
 		List<Integer> dadosDefensor = generarDados(Math.min(defensor.ejercitos, 3));
-		calcularGanador(dadosAtacante, dadosDefensor);
+		int victorias = (int) calcularGanador(dadosAtacante, dadosDefensor).stream()
+		.filter(
+			ganador -> ganador == dadosAtacante
+		).count();
+
+		//es victorioso si derrota tantos dados 
+		//como ejercitos tiene el pais defensor
+		return victorias >= defensor.ejercitos;
 	}
 
-	private int calcularGanador(List<Integer> dadosAtacante, List<Integer> dadosDefensor){
-		return 0;
-	}
+	private List<List<Integer>> calcularGanador(List<Integer> dadosAtacante, List<Integer> dadosDefensor){
+		//ordenar dados de mayor a menor
+		dadosAtacante.sort(Comparator.reverseOrder());
+		dadosDefensor.sort(Comparator.reverseOrder());
 		
-	public List<Integer> generarDados(int soldados){
+		int minTamanio = Math.min(dadosAtacante.size(), dadosDefensor.size());
+
+		//comparar tiros de cada uno en orden descendiente
+		return IntStream.range(0, minTamanio).mapToObj(i -> 
+			dadosAtacante.get(i) > dadosDefensor.get(i) 
+				? dadosAtacante 	
+				: dadosDefensor
+		).collect(Collectors.toList());
+	}
+	
+	//siempre genera dados con el resultado '1'
+	private List<Integer> generarDados(int soldados){
 		List<Integer> dados = new ArrayList<Integer>();
-		for(int i = 0; i < soldados; i++){
-			dados.add ( (int) Math.random() % 6);
-		}
+		for(int i = 0; i < soldados; i++) dados.add (1);
 		return dados;
 	}
 }
---
+++ src/main/test/edu/fiuba/algo3/modelo/PaisTest.java
@@ -0,0 +1,23 @@
+package edu.fiuba.algo3.modelo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PaisTest {
+    @Test
+    public void test01NombrarPais() {
+        Pais pais = new Pais("Estados Unidos");
+        assertEquals("Estados Unidos", pais.obtenerNombre());
+    }
+    @Test
+    public void test01AtacarPais() {
+        Pais atacante = new Pais("Estados Unidos");
+        Pais defensor = new Pais("Canada");
+
+        atacante.agregarEjercitos(3);
+        defensor.agregarEjercitos(3);
+
+        Boolean conquisto = atacante.atacar(new Pais("Canada"), 1);
+        assertEquals("false", conquisto);
+    }
+}
