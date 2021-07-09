30358be54dc704e8001525d58229568187effb4c
mpiligaddi <mgaddi@fi.uba.ar>
2021-07-09 12:54:05 -0300

    agrego clase juego nueva

--- src/main/java/edu/fiuba/algo3/modelo/Juego.java
+++ src/main/java/edu/fiuba/algo3/modelo/Juego.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.stream.Collectors;
 
-//Juego
+//Juego nuevo
 public class Juego {
 	List<Jugador> jugadores;
 	List<Pais> paises = Arrays.asList(
